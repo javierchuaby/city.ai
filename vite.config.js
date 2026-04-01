cd9c472d6664970b067e18f4e2c84120369ddea5
Javier Chua <161703315+javierchuaby@users.noreply.github.com>
2026-04-01 16:21:31 +0800

    fix minor bugs

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react()], // Use standard React plugin for Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
